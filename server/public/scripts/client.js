939ef15885b3756f7ee1d8ace4a359ad1cd37a7a
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-07-20 07:44:35 -0500

    added post and like buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,5 @@
 
-
+//initializing controller
 var myApp = angular.module('myApp', []);
 //controller for creating gif post to bind to the view
 myApp.controller('GifController', function() {
@@ -14,10 +14,25 @@ var post = {
   }; // end of this post
 //pushed a post to the gallery array
 gc.gallery.push(post);
+post = {
+    description: 'This is the Steel County Fair in Owatonna, Minnesota!',
+    gifUrl: 'images/fair.gif',
+    showGif: true
+  }; // end of this post
+//pushed a post to the gallery array
+gc.gallery.push(post);
+post = {
+    description: 'Cheers to a great day!',
+    gifUrl: 'images/cheers.gif',
+    showGif: true
+  }; // end of this post
+//pushed a post to the gallery array
+gc.gallery.push(post);
 
 //toggle function changes visibility of a selected gif
 gc.toggleGif = function(galleryIndex){
   gc.gallery[ galleryIndex ].showGif = !gc.gallery[ galleryIndex ].showGif;
 }; //end of toggle function for gifs
 
-});
+
+});// end of controller
